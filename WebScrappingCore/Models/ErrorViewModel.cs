b402dabd6ba95668512471b535ef0b6ff94b8b2f
Sundaram Subramanian <learnwithsundar@gmail.com>
2020-09-18 11:18:44 +0530

    Inital Commit
    Inital Commit - Making base source code

---
+++ WebScrappingCore/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebScrappingCore.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
